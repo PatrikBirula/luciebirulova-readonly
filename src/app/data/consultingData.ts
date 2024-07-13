cb3dd170d39921b9c9965744bfc41a7428b6204b
Patrik Birula <patrik.birula@juicymo.cz>
2024-07-13 20:35:19 +0200

    Update text field

--- src/app/data/consultingData.ts
+++ src/app/data/consultingData.ts
@@ -1,12 +1,27 @@
 export const introduction = [
-  `Účetnictví je pro Vás španělská vesnice.`,
-  `Otravuje Vás číst cokoliv o účetnictví – nejen, že je to nesrozumitelné, ale taky je to hrozná nuda…`,
-  `Máte pocit, že informací je všude tolik, že Vás z nich bolí hlava, nakonec zjišťujete, že v článcích trávíte hodiny času, ale nic užitečného pro Vás jste se nedozvěděli.`,
-  `Na internetu je fůra obecných rad, ale nejste schopni posoudit, zda jsou důvěryhodné a zda je můžete použít na svou situaci.`,
-  `Netušíte, jaké povinnosti spojené s podnikáním se Vás týkají a bojíte se, zda máte vše v pořádku.`,
-  `Nevíte, co všechno po Vás úřady mohou chtít.`,
-  `Nemáte představu, kolik vlastně budete platit na daních a kolik si máte odkládat stranou na jednorázové nedoplatky.`,
-  `Přemýšlíte, zda na odvodech státu neplatíte až příliš mnoho peněz a zda existuje výhodnější cesta.`,
-  `Přemítáte, zda je pro Vás vhodné OSVČ, zda podávat daňové přiznání nebo zvolit paušální daň, založit s.r.o nebo dokonce uvažovat o plátcovství DPH.`,
-  `Chcete začít podnikat, ale absolutně netušíte, co vše musíte oběhat, oznámit a jaké povinnosti si pohlídat v průběhu roku, jak vystavovat doklady, co evidovat…`,
+  `Účetnictví je pro Vás <strong>španělská vesnice</strong>.`,
+  `Otravuje Vás číst cokoliv o účetnictví – nejen, že je to <strong>nesrozumitelné, ale taky je to hrozná nuda…</strong>`,
+  `Máte pocit, <strong>že informací je všude tolik, že Vás z nich bolí hlava</strong>, nakonec zjišťujete, že <strong>v článcích trávíte hodiny času</strong>, ale nic užitečného pro Vás jste se nedozvěděli.`,
+  `Na internetu je fůra <strong>obecných rad</strong>, ale nejste schopni posoudit, zda jsou <strong>důvěryhodné</strong> a zda je můžete <strong>použít na svou situaci</strong>.`,
+  `Netušíte, jaké <strong>povinnosti spojené s podnikáním</strong> se Vás týkají a bojíte se, zda máte vše v pořádku.`,
+  `Nevíte, <strong>co všechno po Vás úřady mohou chtít</strong>.`,
+  `Nemáte představu, <strong>kolik vlastně budete platit na daních</strong> a kolik si máte odkládat stranou na jednorázové nedoplatky.`,
+  `Přemýšlíte, <strong>zda na odvodech státu neplatíte až příliš mnoho peněz</strong> a zda existuje výhodnější cesta.`,
+  `Přemítáte, <strong>zda je pro Vás vhodné OSVČ</strong>, zda podávat daňové přiznání nebo zvolit paušální daň, <strong>založit s.r.o</strong> nebo dokonce uvažovat o <strong>plátcovství DPH</strong>.`,
+  `Chcete začít podnikat, ale absolutně netušíte, <strong>co vše musíte oběhat, oznámit a jaké povinnosti si pohlídat</strong> v průběhu roku, jak vystavovat doklady, co evidovat…`,
+];
+
+export const ideas = [
+  `Co kdybyste mohli <strong>šetřit čas, peníze a nervy</strong> tím, že se prostě zeptáte, než abyste po večerech luštili články nebo zákony?`,
+  `Co kdybyste měli možnost <strong>získat odpovědi na všechny Vaše otázky</strong>, které v účetnictví řešíte?`,
+  `Co kdybyste věděli, že se můžete kdykoli obrátit na parťáka, který Vám poradí, <strong>jak konkrétní situaci krok za krokem vyřešit</strong>?`,
+  `Co kdyby Vám někdo <strong>polopaticky vysvětlil</strong>, jak účetní nezbytnosti, které potřebujete pochopit, fungují?`,
+];
+
+export const individualConsulting = [
+`Budete mít jistotu, že získáte <strong>odbornou radu podloženou aktuální legislativou</strong>.`,
+`Získáte <strong>konkrétní řešení Vaší situace</strong>, žádné obecné definice.`,
+`<strong>Ušetříte drahocenný čas</strong>, který můžete věnovat naplno svému podnikání nebo rodině.`,
+`Otevřete dveře novým možnostem, <strong>jak nastavit účetní strukturu Vašeho podnikání z pohledu odvodů státu</strong>.`,
+`A mnohé další…`
 ];
--- src/app/poradenstvi/page.tsx
+++ src/app/poradenstvi/page.tsx
@@ -4,6 +4,8 @@ import Footer from "../components/Footer/footer";
 import BulletList from "../components/BulletList/bulletList";
 import {
   introduction,
+  ideas,
+  individualConsulting,
 } from "../data/consultingData";
 
 const coursesRoute = () => {
@@ -38,6 +40,60 @@ const coursesRoute = () => {
       <section className="container page-consulting text-center">
         <h2 className="color-green">Něco o Vás vím...</h2>
         <BulletList items={introduction} />
+      </section>
+      <section className="container page-consulting text-center">
+        <h2 className="color-green">Představte si</h2>
+        <BulletList items={ideas} />
+      </section>
+      <section className="container page-consulting text-center">
+        <h2 className="color-green">Díky osobnímu poradenství</h2>
+        <BulletList items={individualConsulting} />
+      </section>
+      <section className="container page-consulting">
+        <h2 className="color-green text-center">Časté dotazy podnikatelů</h2>
+        <h4 className="text-center">
+          <strong>Proč chtít individuální poradenství</strong>, <br /> když si
+          můžu dnes najít spoustu informací na internetu?
+        </h4>
+        <p>
+          Internet je nesmírně bohatý zdroj informací, ale hledání v něm je,
+          speciálně u tohoto oboru, velmi časově náročné. V článcích můžete
+          nakonec strávit hodiny a hrozí, že získáte nesprávné nebo zavádějící
+          informace, což může mít negativní důsledky ve vztahu k úřadům. Na
+          konzultaci s odborníkem strávíte mnohem méně času, než pátráním na
+          internetu. A čas jsou ….
+        </p>
+        <h4 className="text-center">
+          <strong>Nechce se mi za poradenství platit</strong>, zdá se mi to jako
+          hodně peněz…
+        </h4>
+        <p>
+          Je pravda, že poradenské služby nejsou levnou záležitostí. Představte
+          si ale, že{" "}
+          <strong>
+            díky těmto službám ušetříte na daních a odvodech několikanásobek
+            této částky
+          </strong>
+          , dost často už v prvním roce, kdy aplikujete opatření, které jste se
+          dozvěděli.
+        </p>
+        <p>
+          <strong>
+            Účetnictví je navíc obor, ve kterém dochází velmi často ke změnám
+          </strong>
+          . Pro podnikatele je těžké vše sledovat. Díky poradenství budete mít{" "}
+          <strong>
+            klid, že máte vše z účetního hlediska nastavené správně, a
+            eliminujete tak riziko pokut
+          </strong>{" "}
+          ze strany úřadů.
+        </p>
+        <p>
+          Investice do poradenských služeb je tedy{" "}
+          <strong>investicí do budoucnosti Vašeho podnikání</strong>. Mým cílem
+          je pomoci Vám dosáhnout finanční stability a růstu, což Vám v konečném
+          důsledku přinese více peněz, než kolik za mé služby zaplatíte.
+        </p>
       </section>
       <Footer />
     </>
