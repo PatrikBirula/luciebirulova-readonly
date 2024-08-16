10b7a92851a0f82324652ca1bd3db7d6ae1e1bff
Patrik Birula <patrik.birula@juicymo.cz>
2024-08-16 16:54:41 +0200

    Comment remove pages

--- src/odebrané stránky/ebooks/odebrané_page.tsx
+++ src/odebrané stránky/ebooks/odebrané_page.tsx
@@ -1,22 +1,22 @@
-import "../styles/index.scss";
-import Navbar from "../components/Navbar/navbar";
-import Footer from "../components/Footer/footer";
-import ProductReview from "../components/ProductReview/productReview";
-import { ebooksData } from "../data/ebooksData";
+// import "../styles/index.scss";
+// import Navbar from "../components/Navbar/navbar";
+// import Footer from "../components/Footer/footer";
+// import ProductReview from "../components/ProductReview/productReview";
+// import { ebooksData } from "../data/ebooksData";
 
-const ebooksRoute = () => {
-  return (
-    <>
-      <Navbar />
-      <h1 className="text-center">E-booky</h1>
-      <section className="ebooks">
-        {ebooksData.map((ebook, index) => (
-          <ProductReview key={index} {...ebook} />
-        ))}
-      </section>
-      <Footer />
-    </>
-  );
-};
+// const ebooksRoute = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <h1 className="text-center">E-booky</h1>
+//       <section className="ebooks">
+//         {ebooksData.map((ebook, index) => (
+//           <ProductReview key={index} {...ebook} />
+//         ))}
+//       </section>
+//       <Footer />
+//     </>
+//   );
+// };
 
-export default ebooksRoute;
+// export default ebooksRoute;
